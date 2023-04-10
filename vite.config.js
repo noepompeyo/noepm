768d4cdd334d7af49c7924e7a94ce3c56fcb7a94
noepm <pompeyonoe@gmail.com>
2023-04-09 21:59:00 -0600

    base update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://noepompeyo.github.io/noepm/',
   server: {
     watch: {
       usePolling: true
