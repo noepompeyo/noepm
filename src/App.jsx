a99650feea887f227626faaf9fcb595e3ee4eb31
noepm <pompeyonoe@gmail.com>
2023-04-10 08:05:12 -0600

    Imagen del repositorio

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         <Image
         boxShadow="2xl"
         boxSize='130px'
-        src='/public/noepompeyo.png'
+        src='https://github.com/noepompeyo/noepm/blob/main/public/noepompeyo.png'
         objectFit="cover"
         alt='Noe pompeyo'
         borderRadius="16px"
