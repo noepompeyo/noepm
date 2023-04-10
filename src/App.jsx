2be47899ca354d9a155a1175851bd7c251f7108e
noepm <pompeyonoe@gmail.com>
2023-04-09 21:47:43 -0600

    Traget listo

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   </nav>
   <main>
     <Box width={["300px", "580px"]} mt={4} >
-      <Flex borderRadius="8px 8px 0 0" bg="white" p={4}>
+      <Flex borderRadius="16px 16px 0 0" bg="white" p={4}>
         <Box >
           <Box mb={4}>
         <Text fontSize="lg">Noe Pompeyo</Text>
@@ -36,17 +36,17 @@ function App() {
         />
       </Flex>      
         <Box bg="whiteAlpha.900" p={4}>
-        <Text>Mexicano, actualmente viviendo en Guadalajara Jalisco, soy autodidacta en aprender tecnologias para crear en la web, y me encantaria compartir mis aprendizajes en las redes sociales</Text>
+        <Text>Mexicano, actualmente viviendo en Guadalajara Jalisco, soy autodidacta en aprender tecnologias para crear en la web, y me encantaria compartir mis aprendizajes en las redes sociales.</Text>
         <HStack mt={3}> 
-          <IconButton variant="outline" icon={<BsInstagram/>}/>
-          <IconButton variant="outline" icon={<BsFacebook/>}/>
-          <IconButton variant="outline" icon={<BsTwitter/>}/>
-          <IconButton variant="outline" icon={<BsGithub/>}/>
+          <IconButton fontSize="2xl"  variant="outline" icon={<BsInstagram/>} _hover={{color:"pink.600"}}/>
+          <IconButton fontSize="2xl" variant="outline" icon={<BsFacebook/>}  _hover={{color:"cyan.500"}}/>
+          <IconButton fontSize="2xl" variant="outline" icon={<BsTwitter/>}  _hover={{color:"blue.400"}}/>
+          <IconButton fontSize="2xl" variant="outline" icon={<BsGithub/>}  _hover={{color:"gray.600"}}/>
         </HStack>
         </Box>
       <Box >
-        <Box w="100%" h="50px" bg="orange.300" display="flex" alignItems="center" px={4}>
-          <Link color="orange.600" href='noepm.vercel.app' fontSize="1.2rem" fontWeight="bold" fontFamily="monospace">Blog</Link>
+        <Box w="100%" h="60px" bg="orange.200" display="flex" alignItems="center" px={4} borderRadius="0 0 16px 16px">
+          <Link color="orange.500" href='https://noepm.vercel.app' fontSize="1.2rem" fontWeight="bold" fontFamily="monospace" _hover={{color:'orange.700'}} textDecoration="none">Blog</Link>
           <Spacer/>
           <Text>📖</Text>
         </Box>
@@ -57,7 +57,7 @@ function App() {
     
   </main>
   <footer>
-    <Box>hola </Box>
+    <Box mt={['48px', '180px']}> <Text color="white">Noepm 2023 👩‍🚀 Nice To  </Text></Box>
   </footer>
   </VStack>
   </>
